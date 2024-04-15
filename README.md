5341c0ed4ba9a0f300a0416c5e0c1285d58ae2bb
Prem Puttegowda <pkpgowda@gmail.com>
2024-04-15 15:14:35 -0700

    created template.py and empty files

--- README.md
+++ README.md
@@ -22,3 +22,12 @@ conda activate llama_cpu
 pip install -r requirements.txt
 ```
 
+# Download quantize model from the link provided and keep the model in the model directory:
+
+
+## Download the Llama 2 Model:
+
+llama-2-7b-chat.ggmlv3.q4_0.bin
+from link:
+https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/tree/main
+
---
+++ template.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+import logging
+logging.basicConfig(level=logging.INFO, format='[%s(asctime)s]: %(message)s:')
+
+list_of_files = [
+    'src/_init_.py',
+    'src/run_local.py',
+    'src/helper.py',
+    'model/instruction.py',
+    'requirements.txt',
+    'setup.py',
+    'main.py',
+    'research/trials.ipynb',
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != '':
+        os.makedirs(filedir, exist_ok=True)
+
+    if not filepath.exists():
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f'File created: {filepath}')
+    else:
+        logging.info(f'File already exists: {filepath}')
